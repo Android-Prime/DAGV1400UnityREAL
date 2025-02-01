da7f70471cfd88ca1b42e7a97f1a5e9c602e6239
Andrew <11071850@uvu.edu>
2025-01-31 19:30:59 -0700

    Collider/RigidBody
    Scripting 3 assignment

---
+++ The Sandbox/Assets/Scripts/ThingScriptTime.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class ThingScriptTime : MonoBehaviour
+{
+    //Start is called before the first frame updaye
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        HealthCheck();
+    }
+
+    void HealthCheck()
+    {
+        //check the health of the player
+    }
+}
+   

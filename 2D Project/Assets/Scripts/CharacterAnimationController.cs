7fb3dc2cf95f4c83d753caca12059442ba85b205
Andrew <11071850@uvu.edu>
2025-02-19 21:19:36 -0700

    scripting 5
    basics of animation in unity

---
+++ 2D Project/Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("DoubleJumpTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetTrigger("HitTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("FallTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+    }
+
+}

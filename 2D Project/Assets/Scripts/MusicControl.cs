5ce9531c7c96603b56030bc35015cbc785b12ef6
Andrew <11071850@uvu.edu>
2025-03-22 22:16:56 -0600

    Scripting 9
    Implement audio effects using C# scripts

---
+++ 2D Project/Assets/Scripts/MusicControl.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    public void ToggleMusic()
+    {
+        if (musicSource.isPlaying)
+        {
+            musicSource.Pause();
+        }
+        else
+        {
+            musicSource.Play();
+        }
+    }
+
+}
